b5e6525c1902b5215ab030890d8700eb8efba6fa
pscook25 <p_s_cook@hotmail.com>
2025-04-12 21:17:38 +0800

    Update AVDCustomScript.ps1

--- AVDCustomScript.ps1
+++ AVDCustomScript.ps1
@@ -1,10 +1,10 @@
 #Set FSLogix
 New-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "VHDLocations" -Value "\\stbwavdprof01.file.core.windows.net\avdprofiles\fslogix" -PropertyType MultiString -Force
-New-ItemProperty -Path HKLM:\SOFTWARE\FSLogix\Profiles -Name "AccessNetworkAsComputerObject" -Value 1 -Force
-Set-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "ProfileType" -Type "Dword" -Value "0" -Force
-Remove-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "SIDDirNamePattern" -Force
-Remove-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "SIDDirNameMatch" -Force
-Remove-ItemProperty -Path HKLM:\SOFTWARE\FSLogix\Profiles -Name "CCDLocations"
+#New-ItemProperty -Path HKLM:\SOFTWARE\FSLogix\Profiles -Name "AccessNetworkAsComputerObject" -Value 1 -Force
+#Set-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "ProfileType" -Type "Dword" -Value "0" -Force
+#Remove-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "SIDDirNamePattern" -Force
+#Remove-ItemProperty -Path HKLM:\Software\FSLogix\Profiles -Name "SIDDirNameMatch" -Force
+#Remove-ItemProperty -Path HKLM:\SOFTWARE\FSLogix\Profiles -Name "CCDLocations"
 
 
 #Set Firewall
@@ -14,14 +14,14 @@ New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Netwo
 
 
 #Set AU
-Set-WinSystemLocale en-AU
-Set-Culture -CultureInfo en-AU
-Set-WinHomeLocation -GeoId 12
-Set-TimeZone -Name "W. Australia Standard Time"
-$list = New-WinUserLanguageList en-AU
-$list[0].Handwriting = $true
-Set-WinUserLanguageList $list -Force
-Copy-UserInternationalSettingsToSystem -WelcomeScreen $True -NewUser $True
+#Set-WinSystemLocale en-AU
+#Set-Culture -CultureInfo en-AU
+#Set-WinHomeLocation -GeoId 12
+#Set-TimeZone -Name "W. Australia Standard Time"
+#$list = New-WinUserLanguageList en-AU
+#$list[0].Handwriting = $true
+#Set-WinUserLanguageList $list -Force
+#Copy-UserInternationalSettingsToSystem -WelcomeScreen $True -NewUser $True
 
 
 #Set DNS
