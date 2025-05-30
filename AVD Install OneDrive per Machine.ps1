b2d92708eee1cacf3c26a951442f092ffe2c52d6
pscook25 <p_s_cook@hotmail.com>
2025-05-30 18:52:49 +0800

    Update AVD Install OneDrive per Machine.ps1

--- AVD Install OneDrive per Machine.ps1
+++ AVD Install OneDrive per Machine.ps1
@@ -12,19 +12,45 @@ if (!(Test-Path -Path "C:\Temp")) {
 # Download the OneDriveSetup.exe file
 Invoke-WebRequest -Uri $OneDriveSetupUrl -OutFile $DownloadPath
 
-# Run this command from an elevated command prompt to set the AllUsersInstall registry value
-REG ADD "HKLM\Software\Microsoft\OneDrive" /v "AllUsersInstall" /t REG_DWORD /d 1 /reg:64
+# Registry values to add if they don't exist
+$RegistryValues = @(
+    @{
+        Path = "HKLM:\Software\Microsoft\OneDrive"
+        Name = "AllUsersInstall"
+        Value = 1
+        Type = "DWord"
+    },
+    @{
+        Path = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run"
+        Name = "OneDrive"
+        Value = "C:\Program Files\Microsoft OneDrive\OneDrive.exe /background"
+        Type = "String"
+    },
+    @{
+        Path = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
+        Name = "SilentAccountConfig"
+        Value = 1
+        Type = "DWord"
+    },
+    @{
+        Path = "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive"
+        Name = "KFMSilentOptIn"
+        Value = "703cd1ec-08da-462d-9517-8203ec2c2732"
+        Type = "String"
+    }
+)
+
+# Set registry values only if they don't exist
+foreach ($reg in $RegistryValues) {
+    if (-not (Test-Path $reg.Path)) {
+        New-Item -Path $reg.Path -Force | Out-Null
+    }
+
+    $existingValue = Get-ItemProperty -Path $reg.Path -Name $reg.Name -ErrorAction SilentlyContinue
+    if ($null -eq $existingValue) {
+        Set-ItemProperty -Path $reg.Path -Name $reg.Name -Value $reg.Value -Type $reg.Type
+    }
+}
 
 # Execute the OneDriveSetup.exe file with the /allusers flag
 Start-Process -FilePath $DownloadPath -ArgumentList "/allusers" -Wait
-
-# Run this command to configure OneDrive to start at sign in for all users
-REG ADD "HKLM\Software\Microsoft\Windows\CurrentVersion\Run" /v OneDrive /t REG_SZ /d "C:\Program Files\Microsoft OneDrive\OneDrive.exe /background" /f
-
-# Enable Silently configure user account by running the following command
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v "SilentAccountConfig" /t REG_DWORD /d 1 /f
-
-# Redirect and move Windows known folders to OneDrive by running the following command.
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\OneDrive" /v "KFMSilentOptIn" /t REG_SZ /d "703cd1ec-08da-462d-9517-8203ec2c2732" /f
-
-### End Script ###
